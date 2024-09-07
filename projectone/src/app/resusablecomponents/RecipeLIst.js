2df3707a6a94f235b2caec42a6087070155c262f
whytemig <whytemig@gmail.com>
2024-09-07 18:31:02 -0400

    getting recipes details

--- projectone/src/app/resusablecomponents/RecipeLIst.js
+++ projectone/src/app/resusablecomponents/RecipeLIst.js
@@ -16,12 +16,12 @@ export default async function RecipeList({ recipes }) {
       <div className="p-4 mx-auto lg:max-w-6xl sm:max-w-full">
         <h2 className="text-4xl font-bold text-gray-800 mb-12">Recipes</h2>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 m-8">
         {recipes && recipes.length > 0
           ? recipes.map((recipe) => (
               <Link href={`/recipes/${recipe.id}`}>
-                <Card>
-                  <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1 transition-all">
+                <Card key={recipe.name}>
+                  <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
                     <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
                       <Image
                         className="w-full h-full object-cover object-top"
@@ -30,6 +30,21 @@ export default async function RecipeList({ recipes }) {
                         height={100}
                         alt={recipe.name}
                       />
+                    </div>
+                    <div className="p-6 ">
+                      <h3 className="text-lg font-bold text-gray-700">
+                        {recipe.name}
+                      </h3>
+                    </div>
+                    <div className="mt-4 flex items-center flex-wrap gap-2">
+                      <p className="text-lg text-gray-600">
+                        Ratings: {recipe.rating}
+                      </p>
+                      <div className="ml-auto">
+                        <p className="text-lg text-gray-700 font-bold">
+                          {recipe.cuisine}
+                        </p>
+                      </div>
                     </div>
                   </CardContent>
                 </Card>
