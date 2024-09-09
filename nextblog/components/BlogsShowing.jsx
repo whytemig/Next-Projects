70dd0d2e6d81bd005a97e670d2caef32806eddd9
whytemig <whytemig@gmail.com>
2024-09-08 23:09:55 -0400

    bulding the function

--- nextblog/components/BlogsShowing.jsx
+++ nextblog/components/BlogsShowing.jsx
@@ -1,24 +1,22 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "./ui/button";
-
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import DialogModal from "./DialogModal";
 
 export default function BlogsShowing() {
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [blogsAdded, setBlogsAdded] = useState([]);
 
   return (
     <div className="min-h-screen flex flex-col bg-slate-700 p-6 gap-6">
-      <div className="">
-        <Button onClick={() => setOpen(true)} className="w-full">
-          New Blog
-        </Button>
-      </div>
-
-      <DialogModal open={open} setOpen={setOpen} />
+      <DialogModal
+        open={open}
+        setOpen={setOpen}
+        loading={loading}
+        setLoading={setLoading}
+        setBlogsAdded={setBlogsAdded}
+      />
 
       <div>
         <h2 className="font-bold text-3xl text-slate-300">List of Blogs</h2>
--- nextblog/components/DialogModal.jsx
+++ nextblog/components/DialogModal.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Dialog,
   DialogContent,
@@ -10,32 +11,47 @@ import {
 import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
-export default function DialogModal({ open, setOpen }) {
+export default function DialogModal({
+  open,
+  setOpen,
+  loading,
+  setLoading,
+  blogsAdded,
+  setBlogsAdded,
+}) {
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add New Blog</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title" className="text-right">
-              Title
-            </Label>
-            <Input id="title" className="col-span-3" />
+    <>
+      <div className="">
+        <Button onClick={() => setOpen(true)} className="w-full">
+          New Blog
+        </Button>
+      </div>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add New Blog</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="title" className="text-right">
+                Title
+              </Label>
+              <Input id="title" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input id="description" className="col-span-3" />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input id="description" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Add</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          <DialogFooter>
+            <Button type="submit">Add</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
