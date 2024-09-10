f68ad32645f617b23c7f1757622c39df3b3b8b52
whytemig <whytemig@gmail.com>
2024-09-10 18:33:11 -0400

    final api practice

--- nextblog/components/BlogOverview.jsx
+++ nextblog/components/BlogOverview.jsx
@@ -1,11 +1,23 @@
-// import Card from "@/components/ui/"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function BlogOverview({ blogList }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
       {blogList &&
         blogList?.data.length > 0 &&
-        blogList?.data.map((item) => <div key={item._id}>{item.title}</div>)}
+        blogList?.data.map((item) => (
+          <Card key={item._id} className="p-5 mb-5">
+            <CardContent>{item.title}</CardContent>
+            <CardDescription>{item.description}</CardDescription>
+          </Card>
+        ))}
     </div>
   );
 }
